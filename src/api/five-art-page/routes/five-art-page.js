728994f0302d0756e50775bf9908ec2aecf7ca97
ibnucholdun <ibnucholdun023@gmail.com>
2025-02-17 13:37:59 +0700

    feat: add single-type 5 art page

---
+++ src/api/five-art-page/routes/five-art-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * five-art-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::five-art-page.five-art-page');
