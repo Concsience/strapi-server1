34d721cbd8603743c1bf2f0a8657029a8321c969
Abi-nash <abinash.aki@gmail.com>
2025-03-27 11:40:51 +0545

    upload to object storage bucket

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,32 @@
-module.exports = () => ({});
+// ./config/plugins.js
+module.exports = ({
+  env
+}) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        baseUrl: env('STRAPI_UPLOAD_BASE_URL'),
+        rootPath: '',
+        s3Options: {
+          endpoint: env("STRAPI_UPLOAD_ENDPOINT"),
+          credentials: {
+            accessKeyId: env('STRAPI_UPLOAD_ACCESS_KEY_ID'),
+            secretAccessKey: env('STRAPI_UPLOAD_SECRET_ACCESS_KEY'),
+          },
+          region: env('STRAPI_UPLOAD_REGION'),
+          params: {
+            ACL: 'public-read',
+            signedUrlExpires: 15 * 60,
+            Bucket: env('STRAPI_UPLOAD_BUCKET'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
